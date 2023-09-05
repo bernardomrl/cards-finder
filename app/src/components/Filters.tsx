2395fcc4d1692ea73371b0c6fae0b0d80fcae8b7
Bernardo Meirelles <111180963+bernardomrl@users.noreply.github.com>
2023-09-05 11:01:40 +0000

    Alterado botões dos filtros

--- app/src/components/Filters.tsx
+++ app/src/components/Filters.tsx
@@ -1,24 +1,29 @@
-import { ElixirIcon, RarityIcon, ArenaIcon, TypeIcon } from "@/components/icons";
-
 const filters = [
-    { name: 'Elixir', icon: ElixirIcon },
-    { name: 'Raridade', icon: RarityIcon },
-    { name: 'Arena', icon: ArenaIcon },
-    { name: 'Tipo', icon: TypeIcon }
+    { name: 'Elixir', value: 'elixir', color: 'purple' },
+    { name: 'Raridade', value: 'rarity', color: 'blue' },
+    { name: 'Arena', value: 'arena', color: 'red' },
+    { name: 'Tipo', value: 'type', color: 'green' }
 ]
 
 export default function Filters() {
     return (
-        <div className="flex flex-col w-full max-w-screen-md pl-6">
+        <div className="flex flex-col w-full max-w-md pl-6">
             <h1 className="text-gray-900 dark:text-gray-100 font-poppins font-semibold text-xl my-8">Filtros</h1>
-            <div className="flex items-center space-x-10">
-                {filters.map((filter) => (
-                    <button className="text-center p-4 flex flex-col items-center justify-center border border-gray-500 rounded-xl text-gray-500 text-base dark:text-gray-700 font-medium font-inter">
-                        <filter.icon className="h-6 w-6 mb-2 text-gray-300" />
-                        {filter.name}
-                    </button>
-                ))}
+            <div className="flex items-center justify-between w-full ">
+                <button className='bg-purple-200 text-purple-800 font-inter font-bold text-xs mr-2 px-4 py-1 transition-colors rounded-full dark:bg-purple-900 dark:text-purple-300 hover:bg-purple-300'>
+                    Elixir
+                </button>
+                <button className='bg-blue-200 text-blue-800 font-inter font-bold text-xs mr-2 px-4 py-1 transition-colors rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-300'>
+                    Raridade
+                </button>
+                <button className='bg-red-200 text-red-800 font-inter font-bold text-xs mr-2 px-4 py-1 transition-colors rounded-full dark:bg-red-900 dark:text-red-300 hover:bg-red-300'>
+                    Arena
+                </button>
+                <button className='bg-green-200 text-green-800 font-inter font-bold text-xs mr-2 px-4 py-1 transition-colors rounded-full dark:bg-green-900 dark:text-green-300 hover:bg-green-300'>
+                    Tipo
+                </button>
             </div>
         </div>
+
     )
 }
