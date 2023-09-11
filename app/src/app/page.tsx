321997e36259437aea89034a070824871772d33a
Bernardo Meirelles <111180963+bernardomrl@users.noreply.github.com>
2023-09-11 14:23:05 +0000

    Adicionado componente Results.

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,11 +1,14 @@
-import { Header, Search, Filters } from "@/components";
+import { Header, Search, Filters, Results } from "@/components";
 
 export default function Index() {
     return (
         <>
             <Header />
-            <Search />
-            <Filters />
+            <div className="flex flex-col space-y-6 mt-10">
+                <Search />
+                <Filters />
+                <Results />
+            </div>
         </>
     )
 }
