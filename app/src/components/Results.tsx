189ce37d78262f2de9d77708eb41109750771a10
Viviane Storck <94617183+vihstorck@users.noreply.github.com>
2023-09-11 14:54:18 +0000

    Adicionar cards

--- app/src/components/Results.tsx
+++ app/src/components/Results.tsx
@@ -1,9 +1,18 @@
 'use client';
 import { useState, useEffect } from 'react';
 
+interface Card {
+    key: string;
+    name: string;
+    elixir: number;
+    type: string;
+    rarity: string;
+    arena: number;
+}
+
 export default function Results() {
 
-    const [cards, setCards] = useState([]);
+    const [cards, setCards] = useState<Card[]>([]);
 
     useEffect(() => {
         fetch('https://royaleapi.github.io/cr-api-data/json/cards.json')
@@ -22,17 +31,30 @@ export default function Results() {
     }, [])
 
     return (
-        <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-            <a href="#">
-                <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">Noteworthy technology acquisitions 2021</h5>
-            </a>
-            <p className="mb-3 font-normal text-gray-700 dark:text-gray-400">Here are the biggest enterprise technology acquisitions of 2021 so far, in reverse chronological order.</p>
-            <a href="#" className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-                Read more
-                <svg className="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
-                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
-                </svg>
-            </a>
+        <div className="w-full">
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-6 gap-x-4 gap-y-4">
+                {cards.map(card => (
+                    <div key={card.key} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                        <a href="#">
+                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{card.name}</h5>
+                        </a>
+                        <div className="flex justify-between">
+                            <p className='bg-purple-200 text-purple-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-purple-900 dark:text-purple-300 hover:bg-purple-300 w-max'>
+                                {card.elixir}
+                            </p>
+                            <p className='bg-blue-200 text-blue-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-300 w-max'>
+                                {card.rarity}
+                            </p>
+                            <p className='bg-red-200 text-red-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-red-900 dark:text-red-300 hover:bg-red-300 w-max'>
+                                {card.arena}
+                            </p>
+                            <p className="flex flex-row items-center justify-center space-x-2 bg-green-200 text-green-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-green-900 dark:text-green-300 hover:bg-green-300 w-max">
+                                {card.type}
+                            </p>
+                        </div>
+                    </div>
+                ))}
+            </div>
         </div>
     )
 }
