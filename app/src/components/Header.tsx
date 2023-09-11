7c887d3fea2456e78b4132e38f83454ffcc83401
bernardomrl <bernardomrl@icloud.com>
2023-09-11 16:08:42 -0300

    Correções na estilização do header para atender o daisyui

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,11 +1,14 @@
 export default function Header() {
     return (
-        <header className="shadow-md flex justify-between items-center px-6 py-4">
-            <h1 className="text-gray-900 dark:text-gray-100 font-semibold font-poppins text-xl">API: Clash Royale</h1>
-            <li className="flex justify-between items-center font-poppins space-x-4">
-                <a href="https://github.com/bernardomrl" className="text-gray-900 dark:text-gray-100 font-medium transition-opacity hover:opacity-75 underline underline-offset-1">@bernardomrl</a>
-                <a href="https://github.com/vihstorck" className="text-gray-900 dark:text-gray-100 font-medium transition-opacity hover:opacity-75 underline underline-offset-1">@vihstorck</a>
-            </li>
+        <header className="navbar bg-base-300 p-2">
+            <div className="flex-1">
+                <a href="/" className="btn btn-ghost normal-case text-xl">
+                    CardsFinder
+                </a>
+            </div>
+            <div className="flex-none space-x-4 p-2">
+                <a href="https://github.com/bernardomrl/atividade-framework" className="btn btn-ghost hover:text-primary normal-case font-inter">Repositório GitHub</a>
+            </div> 
         </header>
     )
 }
