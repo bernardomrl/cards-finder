88813bced6e8fb5fd43aa321e795972f93fc93b0
bernardomrl <bernardomrl@icloud.com>
2023-09-11 16:06:33 -0300

    Remover classNames do body.

--- app/src/app/layout.tsx
+++ app/src/app/layout.tsx
@@ -3,7 +3,7 @@ import "@/styles/globals.css";
 export default function RootLayout({ children, }: { children: React.ReactNode }) {
   return (
     <html lang="pt-br">
-      <body className="bg-gray-50 dark:bg-gray-950">{children}</body>
+      <body>{children}</body>
     </html>
   )
 }
