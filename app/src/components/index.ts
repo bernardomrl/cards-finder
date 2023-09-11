b417b015ba38ed27fe0a18492368cdb0d12ee8fb
Bernardo Meirelles <111180963+bernardomrl@users.noreply.github.com>
2023-09-11 14:23:45 +0000

    Adicionado o export de Results

--- app/src/components/index.ts
+++ app/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as Header } from "@/components/Header";
 export { default as Search } from "@/components/Search";
 export { default as Filters } from "@/components/Filters";
+export { default as Results } from "@/components/Results";
