db3f954a5121c058d04ecc2e47692f07bd8bdf6d
Bernardo Meirelles <111180963+bernardomrl@users.noreply.github.com>
2023-09-11 14:24:30 +0000

    Apagar Icones

--- app/src/components/icons/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as ElixirIcon } from "@/components/icons/ElixirIcon";
-export { default as RarityIcon } from "@/components/icons/RarityIcon";
-export { default as ArenaIcon } from "@/components/icons/ArenaIcon";
-export { default as TypeIcon } from "@/components/icons/TypeIcon";
