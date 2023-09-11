bf11e93c0903df9840df30a9706194a564fc174e
bernardomrl <bernardomrl@icloud.com>
2023-09-11 16:07:30 -0300

    Adicionada logica para solicitar dados da API.

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -1,13 +1,48 @@
+'use client';
 import { Header, Search, Filters, Results } from "@/components";
+import { useState, useEffect } from 'react';
+import Card from '@/components/interfaceCard';
 
 export default function Index() {
+    const [cards, setCards] = useState<Card[]>([]);
+    const [sortBy, setSortBy] = useState<string>("");
+    const [searchTerm, setSearchTerm] = useState<string>(''); 
+
+    useEffect(() => {
+        fetch("https://royaleapi.github.io/cr-api-data/json/cards.json")
+            .then((response) => response.json())
+            .then((data) => {
+                const formattedData = data.map(
+                    (card: Card) => ({
+                        key: card.key,
+                        name: card.name,
+                        description: card.description,
+                        elixir: card.elixir,
+                        type: card.type,
+                        rarity: card.rarity,
+                        arena: card.arena,
+                    })
+                );
+                setCards(formattedData);
+            });
+    }, []);
+
+    const handleSortChange = (property: string) => {
+        setSortBy(property);
+    }
+
+    const handleSearch = (searchTerm: string) => {
+        setSearchTerm(searchTerm);
+    }
+
     return (
         <>
             <Header />
-            <div className="flex flex-col space-y-6 mt-10">
-                <Search />
-                <Filters />
-                <Results />
+            <div className="flex flex-col p-6 space-y-8">
+                <Search onSearch={handleSearch}/>
+                <Filters onSortChange={handleSortChange} />
+                <div className="divider"></div>
+                <Results cards={cards} sortBy={sortBy} searchTerm={searchTerm}/>
             </div>
         </>
     )
