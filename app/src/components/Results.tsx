ae8a750bc216a773718925a5c8531f2b9319d266
bernardomrl <bernardomrl@icloud.com>
2023-09-11 16:09:18 -0300

    Adicionada lógica do filtro e atualização das cartas

--- app/src/components/Results.tsx
+++ app/src/components/Results.tsx
@@ -1,60 +1,68 @@
 'use client';
-import { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
+import Card from '@/components/interfaceCard';
 
-interface Card {
-    key: string;
-    name: string;
-    elixir: number;
-    type: string;
-    rarity: string;
-    arena: number;
+interface ResultsProps {
+    cards: Card[];
+    sortBy: string;
+    searchTerm: string;
 }
 
-export default function Results() {
-
-    const [cards, setCards] = useState<Card[]>([]);
+export default function Results({ cards, sortBy, searchTerm }: ResultsProps) {
+    const [sortedCards, setSortedCards] = useState<Card[]>([]);
 
     useEffect(() => {
-        fetch('https://royaleapi.github.io/cr-api-data/json/cards.json')
-            .then(response => response.json())
-            .then(data => {
-                const formattedData = data.map((card: { key: string; name: string; elixir: number; type: string; rarity: string; arena: number; }) => ({
-                    key: card.key,
-                    name: card.name,
-                    elixir: card.elixir,
-                    type: card.type,
-                    rarity: card.rarity,
-                    arena: card.arena
-                }));
-                setCards(formattedData);
+        let newSortedCards = [...cards];
+
+        if (searchTerm) {
+            const lowerCaseSearchTerm = searchTerm.toLowerCase();
+            newSortedCards = newSortedCards.filter((card) => {
+                return card.name.toLowerCase().includes(lowerCaseSearchTerm);
             });
-    }, [])
+        }
+
+        if (sortBy === 'elixir') {
+            newSortedCards.sort((a, b) => a.elixir - b.elixir);
+        } else if (sortBy === 'rarity') {
+            newSortedCards.sort((a, b) => a.rarity.localeCompare(b.rarity));
+        } else if (sortBy === 'arena') {
+            newSortedCards.sort((a, b) => a.arena - b.arena);
+        } else if (sortBy === 'type') {
+            newSortedCards.sort((a, b) => a.type.localeCompare(b.type));
+        }
+
+        setSortedCards(newSortedCards);
+    }, [cards, sortBy, searchTerm]);
 
     return (
-        <div className="w-full">
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 px-6 gap-x-4 gap-y-4">
-                {cards.map(card => (
-                    <div key={card.key} className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
-                        <a href="#">
-                            <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{card.name}</h5>
-                        </a>
-                        <div className="flex justify-between">
-                            <p className='bg-purple-200 text-purple-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-purple-900 dark:text-purple-300 hover:bg-purple-300 w-max'>
-                                {card.elixir}
+        <div className="w-full flex items-center justify-center">
+            <div className="grid grid-cols-1 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-4">
+                {sortedCards.map((card) => (
+                    <div key={card.key} className="card max-w-md shadow-xl bg-base-300">
+                        <div className="card-body flex flex-col space-y-2">
+                            <h2 className="card-title">{card.name}</h2>
+                            <p>
+                                {card.description}
                             </p>
-                            <p className='bg-blue-200 text-blue-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-blue-900 dark:text-blue-300 hover:bg-blue-300 w-max'>
-                                {card.rarity}
-                            </p>
-                            <p className='bg-red-200 text-red-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-red-900 dark:text-red-300 hover:bg-red-300 w-max'>
-                                {card.arena}
-                            </p>
-                            <p className="flex flex-row items-center justify-center space-x-2 bg-green-200 text-green-800 font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full dark:bg-green-900 dark:text-green-300 hover:bg-green-300 w-max">
-                                {card.type}
-                            </p>
+                            <div className="divider"></div>
+                            <div className="card-actions justify-between">
+                                <button className="bg-purple-500/50 text-white font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full w-10">
+                                    {card.elixir}
+                                </button>
+                                <button className="bg-blue-500/50 text-white font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full w-24">
+                                    {card.rarity}
+                                </button>
+                                <button className="bg-red-500/50 text-white font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full w-12">
+                                    {card.arena}
+                                </button>
+                                <button className="bg-green-500/50 text-white font-inter font-bold text-xs px-4 py-1 transition-colors rounded-full w-20">
+                                    {card.type}
+                                </button>
+                            </div>
                         </div>
                     </div>
                 ))}
             </div>
         </div>
-    )
+    );
 }
